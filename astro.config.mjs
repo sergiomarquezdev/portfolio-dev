7c849351bf1306637cf06bb113a677f4f2cc46db
Sergio Márquez <s.marquez@preving.com>
2024-09-04 13:36:04 +0200

    google tag analytics and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,15 @@
 import { defineConfig } from "astro/config";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({});
+export default defineConfig({
+    site: "https://sergiomarquez.dev/",
+    integrations: [
+        sitemap(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            },
+        }),
+    ],
+});
