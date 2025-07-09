73b59adc64a182934c6a972daee06dab81c7aa9e
Sergio Márquez <marquezpsergio@gmail.com>
2025-07-10 00:12:20 +0200

    cleanup: merge Training section and remove unused icons and references

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -19,29 +19,22 @@ export { default as MenuIcon } from "./ui/MenuIcon.astro";
 export { default as ArrowUpIcon } from "./ui/ArrowUpIcon.astro";
 
 // Tech Icons
-export { default as ReactIcon } from "./tech/react.astro";
-export { default as VueIcon } from "./tech/vue.astro";
-export { default as AngularIcon } from "./tech/angular.astro";
 export { default as SpringIcon } from "./tech/spring.astro";
-export { default as TypeScriptIcon } from "./tech/typescript.astro";
-export { default as JavaScriptIcon } from "./tech/javascript.astro";
 export { default as NodeIcon } from "./tech/node.astro";
 export { default as JavaIcon } from "./tech/java.astro";
 export { default as PythonIcon } from "./tech/python.astro";
 export { default as GoIcon } from "./tech/go.astro";
 export { default as DockerIcon } from "./tech/docker.astro";
 export { default as KubernetesIcon } from "./tech/kubernetes.astro";
-export { default as OracleSQLIcon } from "./tech/oraclesql.astro";
 export { default as PostgreSQLIcon } from "./tech/postgresql.astro";
-export { default as MySQLIcon } from "./tech/mysql.astro";
 export { default as MongoDBIcon } from "./tech/mongodb.astro";
-export { default as FirebaseIcon } from "./tech/firebase.astro";
-export { default as AstroIcon } from "./tech/astrosvg.astro";
 export { default as CICDIcon } from "./tech/cicd.astro";
 export { default as AiIcon } from "./tech/ai.astro";
 export { default as LLMOpsIcon } from "./tech/llmops.astro";
 export { default as N8nIcon } from "./tech/n8n.astro";
 export { default as GCPIcon } from "./tech/gcp.astro";
+export { default as FastAPIIcon } from "./tech/fastapi.astro";
+export { default as PineconeIcon } from "./tech/pinecone.astro";
 
 // Social Icons
 export { default as GitHubIcon } from "./social/GitHub.astro";
